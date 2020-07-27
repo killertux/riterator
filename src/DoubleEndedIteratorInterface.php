e092d6ac24588a6cbdb3114e5f2e3475f70bd205
Clemente <bruno.canguru@hotmail.com>
2020-07-27 09:47:44 -0300

    Add docs to Iterators interface

--- src/DoubleEndedIteratorInterface.php
+++ src/DoubleEndedIteratorInterface.php
@@ -5,6 +5,10 @@ namespace RIterator;
 interface DoubleEndedIteratorInterface extends IteratorInterface {
 
 	/**
+	 * Removes and return an element from the end of the iterator
+	 *
+	 * Return and None option if the iteration is finished.
+	 *
 	 * @return Option
 	 */
 	public function nextBack(): Option;
--- src/IteratorInterface.php
+++ src/IteratorInterface.php
@@ -5,6 +5,10 @@ namespace RIterator;
 interface IteratorInterface {
 
 	/**
+	 * Advances the iterator and return the next value wrapped in an Option.
+	 *
+	 * Return and None option if the iteration is finished.
+	 *
 	 * @return Option
 	 */
 	public function next(): Option;
